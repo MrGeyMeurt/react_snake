3fd0274c54a0d62512e177a848814d1b7f368dfd
Manu TEAO <mrgeymeurt@gmail.com>
2024-11-03 00:42:30 +0100

    initial commit

---
+++ src/components/Board/Board.jsx
@@ -0,0 +1,277 @@
+import { useEffect, useRef, useState } from "react";
+import Snake from "../Snake/Snake";
+import gsap from "gsap";
+import s from "./Board.module.scss";
+import Food from "../Food/Food";
+import { generateRandomCoordinates } from "../../utils/utils";
+import GameOver from "../GameOver/GameOver";
+
+const Board = () => {
+    const [snakeData, setSnakeData] = useState([
+        [0, 0],
+        [10, 0],
+    ]);
+    
+    const [foodArray, setFoodArray] = useState([]);
+    const [gameOver, setGameOver] = useState(false);
+    const [speed, setSpeed] = useState(0.2);
+    const [score, setScore] = useState(0);
+    
+    const timer = useRef(0);
+    const foodTimer = useRef(0);
+    const direction = useRef('RIGHT');
+    const canChangeDirection = useRef(true);
+    
+    const gameIsOver = () => {
+        gsap.ticker.remove(gameLoop);
+        
+        setGameOver(true);
+        
+        //console.log("game over") // fin du jeu
+    };
+    
+    const IsOutOfBorder = () => {
+        let head = snakeData[snakeData.length - 1];
+        //console.log(head);
+        
+        if (head[0] >= 500 || head[1] >= 500 || head[0] < 0 || head[1] < 0) {
+            return true;
+        }else{
+            return false;
+        }
+    };
+    
+    const hasEatenFood = () => {
+        //comparer les coordonnée de la  tête du snake avec LES food
+        const head = snakeData[snakeData.length - 1];
+        
+        const food = foodArray.find(_food => _food.x === head[0] &&  _food.y === head[1]);
+        
+        if (food) {
+            //si ya match en renvoie true
+            
+            //mettre à jour le tableau des food disponible
+            const newFoodArray = foodArray.filter(_food => _food !== food);
+            
+            //console.log(newFoodArray);
+            setFoodArray(newFoodArray);
+            
+            return true
+        }else{
+            //sinon en renvoir false
+            return false
+        };
+    }
+    
+    const moveSnake = () => {
+        let newSnakeData = [...snakeData];
+        let head = newSnakeData[newSnakeData.length - 1];
+        //console.log(head); //position de la tete avant changement
+        
+        switch (direction.current) {
+            case 'UP':
+            //console.log("UP");
+            head = [head[0], head[1] - 10];
+            break;
+            case 'DOWN':
+            //console.log("DOWN");
+            head = [head[0], head[1] + 10];
+            break;
+            case 'LEFT':
+            //console.log("LEFT");
+            head = [head[0] - 10, head[1]];
+            break;
+            case 'RIGHT':
+            //console.log("RIGHT");
+            head = [head[0] + 10, head[1]];
+            break;
+            
+            default:
+            break;
+        }
+        
+        newSnakeData.push(head);
+        newSnakeData.shift();
+        
+        const snakeCollapsed = hasCollapsed();
+        const outOfBorder = IsOutOfBorder();
+        const snakeAteFood = hasEatenFood();
+        //console.log(snakeAteFood); // vérif si le head est au mm coordoonée que du food
+        
+        
+        if (outOfBorder || snakeCollapsed) {
+            gameIsOver();
+        } else {
+            if (snakeAteFood === true) {
+                //agrandir le snake
+                newSnakeData.unshift([]);
+                setScore(score + 10);
+                if (speed > 0.05) {
+                    console.log("speed =", speed);
+                    
+                    setSpeed(speed - 0.02);
+                }   
+            }
+            setSnakeData(newSnakeData);
+        }   
+    };
+    
+    const hasCollapsed = () => {
+        let snake = [...snakeData];
+        let head = snake[snake.length - 1];
+        
+        //retire la dernière case du tableau
+        snake.pop();
+        
+        //comparer les coordonnées de la tete avec les autres points du serpent
+        for (let i = 0; i < snake.length; i++) {
+            
+            if (head[0] === snake[i][0] && head[1] === snake[i][1]) {
+                //si match renvoie true
+                return true
+            }
+        }
+        
+        //sinon renvoie false
+        return false;
+    }
+    
+    const onKeyDown = (e) => {
+        //console.log(e);
+        if (canChangeDirection.current === false) return;
+        canChangeDirection.current = false;
+        
+        switch (e.keyCode) {
+            case 90:
+            //console.log("going up");
+            if(direction.current !== "DOWN") {
+                direction.current = "UP";
+            }
+            //Going up
+            break;
+            case 83:
+            if(direction.current !== "UP") {
+                direction.current = "DOWN";
+            }
+            //Going down
+            break;
+            case 81:
+            if(direction.current !== "RIGHT") {
+                direction.current = "LEFT";
+            }
+            //Going left
+            break;
+            case 68:
+            if(direction.current !== "LEFT") {
+                direction.current = "RIGHT";
+            }
+            //Going right
+            break;
+            
+            case 38:
+            if(direction.current !== "DOWN") {
+                direction.current = "UP";
+            }
+            //Going up
+            break;
+            case 40:
+            if(direction.current !== "UP") {
+                direction.current = "DOWN";
+            }
+            //Going down
+            break;
+            case 37:
+            if(direction.current !== "RIGHT") {
+                direction.current = "LEFT";
+            }
+            //Going left
+            break;
+            case 39:
+            if(direction.current !== "LEFT") {
+                direction.current = "RIGHT";
+            }
+            //Going right
+            break;
+            
+            default:
+            break;
+        }
+    };
+    
+    const addFood = () => {
+        //console.log("add food");
+        //génération cooredonnées
+        const coordinates = generateRandomCoordinates();
+        
+        //maj du state
+        setFoodArray((oldFoodArray) => [...oldFoodArray, coordinates]);
+    };
+    
+    const gameLoop = (time, deltaTime, frame) => {
+        //console.log(time, deltaTime, frame);
+        timer.current += deltaTime * 0.001;
+        foodTimer.current += deltaTime * 0.001;
+        //console.log(foodTimer.current); voir le timer de la food
+        
+        if (foodTimer.current > 3 && foodArray.length < 10 ) {
+            foodTimer.current = 0;
+            //console.log("pop");
+            addFood();
+        }
+        
+        if (timer.current > speed) {
+            //console.log("move snake");
+            timer.current = 0;
+            moveSnake();
+            canChangeDirection.current = true;
+        }
+    };
+    
+    const replay = () => {
+        //replay game
+        
+        //reset game
+        setGameOver(false);
+        //reset snake
+        setSnakeData([
+            [0, 0], 
+            [10, 0],
+        ]);
+        //reset food
+        setFoodArray([]);
+        //reset direction
+        direction.current = "RIGHT";
+        //reset timer
+        timer.current = 0;
+        //reset food timer
+        foodTimer.current = 0;
+        setSpeed (0.2);
+        setScore(0)
+    }
+    
+    useEffect(() => {
+        window.addEventListener("keydown",onKeyDown);
+        gsap.ticker.add(gameLoop);
+        
+        return () => {
+            window.removeEventListener("keydown", onKeyDown);
+            gsap.ticker.remove(gameLoop);
+        };
+    }, [snakeData]);
+    
+    return (
+        <div className={s.board}>
+        <Snake data={snakeData} />
+
+        <span className={s.score}>Score: {score}</span>
+        
+        {gameOver && <GameOver replay={replay}/>}
+        
+        {foodArray.map((coordinates) => (
+            <Food key={coordinates.id} coordinates={coordinates}/>
+        ))}
+        </div>
+    );
+};
+
+export default Board;
---
+++ src/components/Snake/Snake.jsx
@@ -0,0 +1,17 @@
+import s from "./Snake.module.scss"
+
+const Snake = ({data}) => {
+    //console.log(data) //verif de data
+    return (
+        <>
+        {data.map((dot, i) => (
+            <div
+            key={i}
+            className={s.snakeDot}
+            style={{transform: `translate(${dot[0]}px, ${dot[1]}px)`}}></div>   
+        ))}
+        </>
+    );
+};
+
+export default Snake;
