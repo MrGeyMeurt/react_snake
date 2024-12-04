f4934bcfa65be1dc832a2d71a0e480ae7eb32593
Manu TEAO <mrgeymeurt@gmail.com>
2024-12-04 20:37:32 +0100

    results | gameover | skin

--- src/utils/store.jsx
+++ src/utils/store.jsx
@@ -1,12 +1,27 @@
 import { create } from 'zustand';
 
 const useStore = create((set) => ({
+    results: [],
+    setResults: (param) =>
+        set(() => ({
+        results: param,
+    })),
+    gameOver: false,
+    setGameOver: (param) =>
+        set(() => ({
+        gameOver: param,
+    })),
+    skin: null,
+    setSkin: (param) =>
+        set(() => ({
+        skin: param,
+    })),
     mod: [],
     addMod: (param) =>
         set((state) => ({
         mod: [...state.mod, param],
     })),
-
+    
     removeMod: (param) =>
         set((state) => ({
         mod: state.mod.filter((_mod) => _mod !== param),
