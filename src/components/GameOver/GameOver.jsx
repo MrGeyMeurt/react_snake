92389e8fbdb9b655ea0fad9ac94ba3b0bcc74442
Manu TEAO <mrgeymeurt@gmail.com>
2024-12-04 20:36:04 +0100

    Update GameOver.jsx

--- src/components/GameOver/GameOver.jsx
+++ src/components/GameOver/GameOver.jsx
@@ -1,12 +1,13 @@
 import s from "./GameOver.module.scss";
 
-const GameOver = ({replay}) => {
-    return (
-      <div className={s.gameOver}>
-      <h1>Game Over</h1>
-      <button onClick={replay}>Replay</button>
-      </div>  
-    );
+const GameOver = ({ replay }) => {
+  return (
+    <div>
+      <button onClick={replay} className={s.btn}>
+        Replay
+      </button>
+    </div>
+  );
 };
 
-export default GameOver
+export default GameOver;
