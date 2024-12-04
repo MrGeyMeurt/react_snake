7b45dc4df714ba52bab001ba2a6d72b9de7ad435
Manu TEAO <mrgeymeurt@gmail.com>
2024-12-04 20:36:43 +0100

    snake skin refacto

--- src/components/Snake/Snake.jsx
+++ src/components/Snake/Snake.jsx
@@ -1,17 +1,35 @@
-import s from "./Snake.module.scss"
+import useStore from "../../utils/store";
+import s from "./Snake.module.scss";
 
-const Snake = ({data}) => {
-    //console.log(data) //verif de data
-    return (
-        <>
-        {data.map((dot, i) => (
-            <div
-            key={i}
-            className={s.snakeDot}
-            style={{transform: `translate(${dot[0]}px, ${dot[1]}px)`}}></div>   
-        ))}
-        </>
-    );
+const Snake = ({ data }) => {
+  const { skin } = useStore();
+
+  // console.log(data);
+
+  const getStyle = (dot, i) => {
+    let background = null;
+
+    if (data[data.length - 1] === dot) {
+      background = `url('/skin.jpg') 0 0`;
+    } else {
+      background = `url('/skin.jpg') ${10 * i}px 10px`;
+    }
+
+    const style = {
+      transform: `translate(${dot[0]}px, ${dot[1]}px)`,
+      background: background,
+    };
+
+    return style;
+  };
+
+  return (
+    <>
+      {data.map((dot, i) => (
+        <div key={i} className={s.snakeDot} style={getStyle(dot, i)}></div>
+      ))}
+    </>
+  );
 };
 
 export default Snake;
